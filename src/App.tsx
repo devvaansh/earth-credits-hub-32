22c590b1b0cc506b703d531d2d887a8a13e038ac
Harsh Kumar <harshmastic@gmail.com>
2025-09-08 21:04:45 +0530

    updated login route

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route 
             path="/ngo-dashboard" 
             element={
